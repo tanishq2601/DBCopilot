5fd0c29aa06c2c8a2324d84eefe05ee40268a966
tanishq2601 <tanishqjain0000@gmail.com>
2025-02-15 13:46:20 +0530

    frontend integration

--- main.py
+++ main.py
@@ -32,3 +32,8 @@ def operator(query):
 def main(query: Query):
     nl_response = operator(query)
     return {"response" : nl_response}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
